9de1293458aabad066fec8e70bc65b5d0e31946a
lyingloong <lyingloong@mail.ustc.edu.cn>
2025-12-23 14:29:30 +0800

    update: models download

---
+++ download/download-sd.py
@@ -0,0 +1,14 @@
+"""
+download sd model
+"""
+import torch
+from diffusers import DiffusionPipeline
+
+model_cache_dir = "./huggingface_models/stable-diffusion-v1-4"
+
+pipe = DiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.bfloat16, device_map="balanced", cache_dir=model_cache_dir)
+
+prompt = "A high tech solarpunk utopia in the Amazon rainforest"
+image = pipe(prompt).images[0]
+image.save("solarpunk_utopia.png")
+
